c06ca9d0ca6dfba371f7e0b298e98f5b3f6a82e6
Carlos Alba <calba114@carloss-mbp.lan>
2019-06-14 11:05:00 -0700

    log

--- scripts/scan.js
+++ scripts/scan.js
@@ -1,7 +1,13 @@
 
 if(window.File && window.FileReader && window.FileList && window.Blob){
-
-}
+  const directory  = 'https://calba5141114.github.io/OpenMed/documents/';
+  fetch(directory).
+  then(response => {
+    console.log(response);
+  });
 else {
   alert('The File APIs are not fully supported in this browser.');
 }
+
+
+// https://calba5141114.github.io/OpenMed/documents/contribution-test.md
