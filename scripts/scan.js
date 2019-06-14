177f695de1a9bedb43840be6c3c9fcbc2523b641
Carlos Alba <calba114@carloss-mbp.lan>
2019-06-14 11:24:54 -0700

    update

--- scripts/scan.js
+++ scripts/scan.js
@@ -4,14 +4,15 @@ async function getMarkdownfiles(){
   const dir  = 'https://calba5141114.github.io/OpenMed/documents/documents.json';
   const response = await fetch(dir);
   const data = await response.json();
-  return data;
+
+  console.log(data);
+
 }
 
 
 
 if(window.File && window.FileReader && window.FileList && window.Blob){
-  const data = getMarkdownfiles();
-  console.log(data[0]);
+  getMarkdownfiles();
 }
 else {
   alert('The File APIs are not fully supported in this browser.');
